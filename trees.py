e0d6126481563da7b5bdb9f0f0a427cf4126fba0
arpitvce <anarpit12@outlook.com>
2026-02-06 20:01:21 +0530

    trees are beautiful

---
+++ trees.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self,val,Left=None,Right=None):
+        self.val=val
+        self.Left=Left
+        self.Right=Right
+def printer(k):
+    if(k==None):
+        return
+    printer(k.Left)
+    print(k.val)
+    printer(k.Right)
+
+
+a=Node(2,None,None)
+b=Node(5,None,None)
+c=Node(8,None,None)
+d=Node(9,None,None)
+e=Node(4,None,None)
+f=Node(5)
+g=Node(3)
+ #       2
+ #     5    8
+ #   9  4  5  3
+a.Left=b
+a.Right=c
+b.Left=d
+b.Right=e
+c.Left=f
+c.Right=g
+printer(a)
+
